ffa152f193c542576614c852be64d70229d1519f
Ajay <mail@ajaymore.in>
2020-04-01 17:40:54 +0530

    Cloud server docs added successfully

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import styles from './styles.module.css';
 
 const features = [
   {
-    title: <>Easy to Use and fun</>,
+    title: <>Easy to Use</>,
     imageUrl: 'img/undraw_docusaurus_mountain.svg',
     description: (
       <>
